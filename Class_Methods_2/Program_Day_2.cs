ae14697695d6a34687b948c90a3c919d04d99095
your username <your email>
2025-07-22 08:56:51 -0400

    Initial_Commit_Day_2

---
+++ Class_Methods_2/Program_Day_2.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// Math getLarge = new Math();
+// int number = getLarge.Max();
+// Console.WriteLine(number);
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+largerValue = Math.Max(firstValue, secondValue);
+Console.WriteLine(largerValue);
