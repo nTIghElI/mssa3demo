a8618db7806415a0e6a60f553f3f98b064d7c0a3
your username <your email>
2025-07-23 16:16:49 -0400

    Final Commit

--- Class_Methods_3_Arrays_Loops/Array.cs
+++ Class_Methods_3_Arrays_Loops/Array.cs
@@ -7,37 +7,57 @@ class Program
         // Console.WriteLine("Hello, World!");
 
         //arrays
-        int[,] grid = new int[3, 3];
+        // int[,] grid = new int[3, 3];
 
-        for (int row = 0; row < grid.GetLength(0); row++)
-        {
-            for (int col = 0; col < grid.GetLength(1); col++)
-            {
-                grid[row, col] = row * 3 + col;
-                Console.WriteLine($"{grid[row, col]}");
-            }
-        }
+        // for (int row = 0; row < grid.GetLength(0); row++)
+        // {
+        //     for (int col = 0; col < grid.GetLength(1); col++)
+        //     {
+        //         grid[row, col] = row * 3 + col;
+        //         Console.WriteLine($"{grid[row, col]}");
+        //     }
+        // }
 
-        //multi-dimensional array - 2d
-        int[][] matrix = new int[3][];
- 
-        for (int i = 0; i < matrix.Length; i++)
-        {
-            matrix[i] = new int[3];
-            for (int j = 0; j < matrix[i].Length; j++)
-            {
-                matrix[i][j] = i * 3 + j;
-            }
-        }
- 
-        int[][] matrix1 =
-        {
-            new int[] {0, 1, 2},
-            new int[] {3, 4, 5},
-            new int[] {6, 7, 8, 9}
-        };
+        // //multi-dimensional array - 2d
+        // int[][] matrix = new int[3][];
+
+        // for (int i = 0; i < matrix.Length; i++)
+        // {
+        //     matrix[i] = new int[3];
+        //     for (int j = 0; j < matrix[i].Length; j++)
+        //     {
+        //         matrix[i][j] = i * 3 + j;
+        //     }
+        // }
+
+        // int[][] matrix1 =
+        // {
+        //     new int[] {0, 1, 2},
+        //     new int[] {3, 4, 5},
+        //     new int[] {6, 7, 8, 9}
+        // };
+
+        // string message = "The quick brown fox jumps over the lazy dog.";
+        // // convert the message into a char array
+        // char[] charMessage = message.ToCharArray();
+        // // Reverse the chars
+        // Array.Reverse(charMessage);
+        // int x = 0;
+        // // count the o's
+        // foreach (char i in charMessage)
+        // {
+        //     if (i == 'o') { x++; }
+        // }
+        // // convert it back to a string
+        // string reverseMessage = new String(charMessage);
+        // // print it out
+        // Console.WriteLine(reverseMessage);
+        // Console.WriteLine($"'o' appears {x} times.");
+
         
 
         //Code here;
     }
 }
+
+
